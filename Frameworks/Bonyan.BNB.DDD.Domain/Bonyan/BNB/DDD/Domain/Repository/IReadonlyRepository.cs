9f94513406d21aecc083d9909a270206a18f19b4
akbaramd <akbarsafari00@gmail.com>
2023-12-20 12:56:59 -0800

    .

---
+++ Frameworks/Bonyan.BNB.DDD.Domain/Bonyan/BNB/DDD/Domain/Repository/IReadonlyRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+using Bonyan.BNB.Domain.Entities;
+
+namespace Bonyan.BNB.DDD.Domain.Repository;
+
+public interface IReadonlyRepository<TEntity, in TKey> where TEntity : IBnbEntity<TKey>
+{
+    Task<List<TEntity>> FindAsync(Expression<Func<TEntity, bool>> filter, CancellationToken cancellationToken = default);
+    Task<TEntity?> FindOneAsync(Expression<Func<TEntity, bool>> filter, CancellationToken cancellationToken = default);
+    Task<TEntity> GetOneAsync(Expression<Func<TEntity, bool>> filter, CancellationToken cancellationToken = default);
+    Task<TEntity?> FindByIdAsync(TKey key, CancellationToken cancellationToken = default);
+    Task<TEntity> GetByIdAsync(TKey key, CancellationToken cancellationToken = default);
+}
---
+++ Frameworks/Bonyan.BNB.DDD.Domain/Bonyan/BNB/DDD/Domain/Repository/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+using Bonyan.BNB.Domain.Entities;
+
+namespace Bonyan.BNB.DDD.Domain.Repository;
+
+public interface IRepository<TEntity, in TKey> : IReadonlyRepository<TEntity,TKey> where TEntity : IBnbEntity<TKey>
+{
+    Task InsertAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default);
+}
